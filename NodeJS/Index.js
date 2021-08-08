7614f917cda70c02e47c4665c28540423f78c5b1
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-08 16:57:18 +0500

    NodeJS

--- NodeJS/Index.js
+++ NodeJS/Index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World to First API');
+})
+
+app.listen(port, () => {
+    console.log('Server is Running!')
+})
