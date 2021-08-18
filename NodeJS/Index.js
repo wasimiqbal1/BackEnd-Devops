fa3b5eb5e703d99c669aa463e880c804ff02aff2
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-18 23:31:45 +0500

    API call

--- NodeJS/Index.js
+++ NodeJS/Index.js
@@ -1,14 +1,22 @@
 const express = require('express');
-
+const cors = require('cors')
+const bd = require('body-parser')
 const app = express();
 const port = 5000;
+
+app.use(cors());
+app.use(bd.urlencoded({
+    extended: false
+}))
+app.use(bd.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to NodeJS');
 })
 
 app.post('/signup', (req, res) => {
-    res.send('Signup API');
+    // res.send('Signup API');
+    console.log(req.body)
 })
 
 
