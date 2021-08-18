a9fb39e18070623cef4c35c7450d738974a556eb
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-18 23:12:45 +0500

    NodeJS post

--- NodeJS/Index.js
+++ NodeJS/Index.js
@@ -4,8 +4,13 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World to First API');
+    res.send('Welcome to NodeJS');
 })
+
+app.post('/signup', (req, res) => {
+    res.send('Signup API');
+})
+
 
 app.listen(port, () => {
     console.log('Server is Running!')
