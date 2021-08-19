9d286e4d684cae51d24513c87dc867d4243476ee
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-19 10:46:54 +0500

    MongoDB Connected

--- NodeJS/Index.js
+++ NodeJS/Index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const bd = require('body-parser')
 const app = express();
+const mongoose = require('mongoose');
 const port = 5000;
 
 app.use(cors());
@@ -10,13 +11,26 @@ app.use(bd.urlencoded({
 }))
 app.use(bd.json());
 
+mongoose.connect('mongodb+srv://wasim:P@ss1234@cluster0.flcnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("Datebase Connected")
+})
+mongoose.connection.on("error", () => {
+    console.log("Datebase Not Connected")
+})
+
 app.get('/', (req, res) => {
     res.send('Welcome to NodeJS');
 })
 
 app.post('/signup', (req, res) => {
     // res.send('Signup API');
-    console.log(req.body)
+    console.log(req.body.post.postName)
 })
 
 
